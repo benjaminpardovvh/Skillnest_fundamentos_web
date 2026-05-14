48c99e23b6d6b1fdf4b250fac34110d5cd6dd36d
benjaminpardovvh <benjaminpardo@liceovvh.cl>
2026-05-14 13:01:34 -0400

    actualizacion

---
+++ STACK_JS/01_Intro_Js/static/JS/script.js
@@ -0,0 +1,10 @@
+console.log("conexión exitosa")
+
+function cambiarTexto() {
+    let titulo = "Hola, bienvenido"
+    if (document.getElementById("titulo").innerText == titulo) 
+    {document.getElementById("titulo").innerText = "Has cambiado el texto con JS"}
+    else {document.getElementById("titulo").innerText = titulo}
+}
+
+//posible solucion: usar "setTimeout()" y "location.reload()"
