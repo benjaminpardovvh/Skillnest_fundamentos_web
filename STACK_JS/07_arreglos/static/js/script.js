56d90f4fa608b145edce8119586fb242674574b8
benjaminpardovvh <benjaminpardo@liceovvh.cl>
2026-07-22 11:09:41 -0400

    actualización

--- STACK_JS/07_arreglos/static/js/script.js
+++ STACK_JS/07_arreglos/static/js/script.js
@@ -68,9 +68,108 @@ function edades() {
 }
 
 function nombresJojo() {
-    let nombres2 = ["pedrito", "juanita", "jonathan", "diego" , "carlitos" , "martin"]
+    let nombres2 = ["pedrito", "juanita", "jonathan", "diego", "carlitos", "martin"]
     for (let i = 0; i <= 5; i++) {
         alert(`hola ${nombres2[i]}`)
     }
 }
 
+function notas() {
+    let notas = [6.2, 5.8, 4.1, 7.0, 5.9];
+    let promedio = 0;
+    for (let i = 0; i < notas.length; i++) {
+        promedio += notas[i]
+    }
+    alert(`Promedio: \n${promedio / 5}`)
+}
+
+function temperatura() {
+    let temperatura = [18, 20, 15, 22, 19, 25, 17]
+    let tempura = 0;
+    for (let i = 0; i < temperatura.length; i++) {
+        if (temperatura[i] > 20) {
+            tempura++
+            alert(temperatura[i])
+        }
+        else { }
+    }
+    alert(`hay un total de ${tempura} temperaturas mayores a 20`)
+}
+
+function clasificador() {
+    let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    for (let i = 0; i < numeros.length; i++) {
+        let clasificador = numeros[i] % 2
+        if (clasificador == 0) {
+            alert(`${numeros[i]} es par`)
+        }
+        else {
+            alert(`${numeros[i]} es impar`)
+        }
+    }
+}
+
+function ventas() {
+    let ventas = [5000, 12000, 3000, 4500, 7000]
+    let ventaMayor = ventas[0]
+    let ventaMenor = ventas[0]
+    let totalVentas = 0
+    for (let i = 0; i < ventas.length; i++) {
+        if (ventas[i] < ventaMenor) {
+            ventaMenor = ventas[i]
+        }
+        else if (ventas[i] > ventaMayor) {
+            ventaMayor = ventas[i]
+        }
+        else {
+            console.log(`valor skipeado: ${ventas[i]}`)
+        }
+        totalVentas += ventas[i];
+    }
+    alert(`Mayor venta: ${ventaMayor}\nMenor venta: ${ventaMenor}\nTotal: ${totalVentas}`)
+}
+
+function nombres4Letras() {
+    let nombres3 = ["Ana", "Pedro", "María", "Juan", "Camila", "Tomás"]
+    for (let i = 0; i < nombres3.length; i++) {
+        if (nombres3[i].length > 4) {
+            alert(nombres3[i])
+        }
+    }
+}
+
+function sumarVentasTotales() {
+    let ventas = [10000, 5000, 12000, 8000];
+    let total = 0;
+    for (let i = 0; i < ventas.length; i++) {
+        total += ventas[i];
+    }
+    console.log(total);
+}
+
+function integrador() {
+    let ventas = [5000 , 8000 , 12000 , 3000 , 10000 , 9000 , 4000];
+    let total = 0;
+    let mayor = ventas[0];
+    let mayoresDe10000 = 0
+    let mayores = []
+    let borrado = ventas.pop()
+    ventas.push(parseInt(prompt(`ingresa un valor para agregar a la cuenta`)))
+    ventas.push(parseInt(prompt(`ingresa un valor para agregar a la cuenta`)))
+    alert(`${ventas}`)
+    for (let i = 0; i < ventas.length; i++) {
+        total += ventas[i];
+        if (ventas[i] > mayor) {
+            mayor = ventas[i];
+            if (ventas[i] >= 10000) {
+                mayores.push(ventas[i])
+                mayoresDe10000++
+            }
+        }
+    }
+    alert(`Valor eliminado: ${borrado}
+Total: ${total}
+Mayor: ${mayor}
+Promedio: ${total / ventas.length}
+Todas las ventas mayores a 10.000: ${mayoresDe10000} (${mayores.join(' - ')})`)
+}
---
+++ STACK_JS/08_desafios_con_arreglos/static/js/script.js
@@ -0,0 +1,63 @@
+console.log(`hola :3`)
+
+function filtrarVideojuegos() {
+    let videojuegos = [
+        { nombre: "Minecraft", edadRecomendada: 7 },
+        { nombre: "FIFA 23", edadRecomendada: 3 }, //me decepciona el hecho que tenga que ir en contra de mis principios como jugador
+        { nombre: "Call of Duty", edadRecomendada: 18 },
+        { nombre: "Fortnite", edadRecomendada: 12 }
+    ];
+    let edadMinima = 12;
+    let recomendados = [];
+    for (let i = 0; i < videojuegos.length; i++) {
+        if (videojuegos[i].edadRecomendada <= edadMinima) {
+            recomendados.push(videojuegos[i].nombre);
+        }
+    }
+    alert("Juegos recomendados: ", recomendados);
+}
+
+//🎵 Contador de canciones favoritas
+function contadorMusica() {
+    let historialReproduccion = [
+        "Shallow",
+        "Blinding Lights",
+        "Shallow",
+        "Anti-Hero",
+        "Shallow",
+        "Bad Habits",
+    ];
+    let cancionBuscada = "Shallow";
+    let contador = 0;
+    for (let i = 0; i < historialReproduccion.length; i++) {
+        if (historialReproduccion[i] === cancionBuscada) {
+            contador++;
+        }
+    }
+    alert("La canción", cancionBuscada, "se reprodujo", contador, "veces.");
+}
+
+//🕹️ Organizadores de torneo de videojuegos
+function torneo() {
+    let jugadores = [
+        "Luis",
+        "Andrea",
+        "Carlos",
+        "Sofía",
+        "Pedro",
+        "Martina",
+        "Javier",
+        "Valentina",
+    ];
+    let tamanoGrupo = 3;
+    let grupos = [];
+    let grupoActual = [];
+    for (let i = 0; i < jugadores.length; i++) {
+        grupoActual.push(jugadores[i]);
+        if (grupoActual.length === tamanoGrupo || i === jugadores.length - 1) {
+            grupos.push(grupoActual);
+            grupoActual = [];
+        }
+    }
+    console.log("Grupos formados: ", grupos);
+}
