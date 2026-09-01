964056af37bcc564e8edd96f3c44b031d79e7404
benjaminpardovvh <benjaminpardo@liceovvh.cl>
2026-09-01 10:48:14 -0400

    HotFix

---
+++ STACK_JS/17_selecctor_querySelector/static/js/script.js
@@ -0,0 +1,8 @@
+console.log("hola")
+
+let p = document.querySelector("p");
+console.log(p); // <p>SELECT</p>
+
+let title = document.querySelector("#title");
+console.log(title); // <h1 id="title">¡Hola, mundo!</h1>
+console.log(`el contenido del titulo es: ${title.textContent}`)
